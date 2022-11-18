c082559040d03cc505a8deafda07afc0652bf395
HoseonLee <hoseon310@gmail.com>
2022-11-18 14:03:12 +0900

    #7.19 Github Login part Four

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
+import fetch from "cross-fetch";
 
 // rootRouter
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
@@ -83,13 +84,27 @@ export const finishGithubLogin = async (req, res) => {
   };
   const params = new URLSearchParams(config).toString();
   const finalURL = `${baseURL}?${params}`;
-  const data = await fetch(finalURL, {
-    method: "POST",
-    headers: {
-      Accept: "application / json",
-    },
-  });
-  const json = await data.json();
+  const tokenRequest = await (
+    await fetch(finalURL, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+  ).json();
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const userRequest = await (
+      await fetch("https://api.github.com/user", {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    console.log(userRequest);
+  } else {
+    return res.redirect("/login");
+  }
 };
 
 export const logout = (req, res) => res.send("Logout");
