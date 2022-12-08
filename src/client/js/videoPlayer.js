49709b708af30b9ca098ef3a9d9b61cdfa8ac7c6
HoseonLee <hoseon310@gmail.com>
2022-12-08 23:51:30 +0900

    11.3 Volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,11 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 
+const volumeDefault = 0.3;
+let volumeUser = volumeDefault;
+volumeRange.value = volumeDefault;
+video.volume = volumeDefault;
+
 playBtn.addEventListener("click", () => {
   if (video.paused) {
     video.play();
@@ -16,9 +21,34 @@ playBtn.addEventListener("click", () => {
 muteBtn.addEventListener("click", () => {
   if (video.muted) {
     video.muted = false;
+    video.volume = volumeUser;
   } else {
     video.muted = true;
   }
   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-  volumeRange.value = video.muted ? "0" : "0.5";
+  volumeRange.value = video.muted ? "0" : volumeUser;
+});
+
+volumeRange.addEventListener("input", (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  } // 볼륨이 0인 이후에 볼륨을 높이는 상황
+  if (value === "0") {
+    video.muted = true;
+    muteBtn.innerText = "Unmute";
+  } // 볼륨이 0인 상황
+  video.volume = value;
+});
+
+volumeRange.addEventListener("change", (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (value !== "0") {
+    volumeUser = value;
+  } // 한번의 드래그로 볼륨을 0으로 만들고 → 다시 음소거를 해체했을 때 → 드래그를 시작한 볼륨으로 돌아가기 위한 if문
 });
