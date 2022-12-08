e590214950cec2119c15ea401216141d857a61e8
HoseonLee <hoseon310@gmail.com>
2022-12-08 10:21:42 +0900

    11.1 Play Pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,18 @@
-console.log("videoPlayer");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+playBtn.addEventListener("click", () => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+});
+
+muteBtn.addEventListener("click", () => {});
+
+video.addEventListener("pause", () => (playBtn.innerText = "Play"));
+video.addEventListener("play", () => (playBtn.innerText = "Pause"));
