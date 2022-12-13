e9b3e5265d96b67f1e8a4ce74a53f98b4c0292fd
HoseonLee <hoseon310@gmail.com>
2022-12-13 13:19:06 +0900

    type: feat
    body
    1. Make new keyboard shortcuts. m, f, spacebar
    2. Add a fullscreenchange event to change the innerText of the fullScreenBtn using the ESC key.
    3. Make new names of event handler functions.

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -14,16 +14,22 @@ let volumeUser = volumeDefault;
 volumeRange.value = volumeDefault;
 video.volume = volumeDefault;
 
-playBtn.addEventListener("click", () => {
+const handlePlayBtnClick = () => {
   if (video.paused) {
     video.play();
   } else {
     video.pause();
   }
   playBtn.innerText = video.paused ? "Play" : "Pause";
+};
+playBtn.addEventListener("click", handlePlayBtnClick);
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Space") {
+    handlePlayBtnClick();
+  }
 });
 
-muteBtn.addEventListener("click", () => {
+const handleMuteBtnClick = () => {
   if (video.muted) {
     video.muted = false;
     video.volume = volumeUser;
@@ -32,9 +38,15 @@ muteBtn.addEventListener("click", () => {
   }
   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
   volumeRange.value = video.muted ? "0" : volumeUser;
+};
+muteBtn.addEventListener("click", handleMuteBtnClick);
+document.addEventListener("keyup", (event) => {
+  if (event.code === "KeyM") {
+    handleMuteBtnClick();
+  }
 });
 
-volumeRange.addEventListener("input", (event) => {
+const handleVolumeRangeInput = (event) => {
   const {
     target: { value },
   } = event;
@@ -47,38 +59,42 @@ volumeRange.addEventListener("input", (event) => {
     muteBtn.innerText = "Unmute";
   } // 볼륨이 0인 상황
   video.volume = value;
-});
-
-volumeRange.addEventListener("change", (event) => {
+};
+const handleVolumeRangeChange = (event) => {
   const {
     target: { value },
   } = event;
   if (value !== "0") {
     volumeUser = value;
   } // 한번의 드래그로 볼륨을 0으로 만들고 → 다시 음소거를 해체했을 때 → 드래그를 시작한 볼륨으로 돌아가기 위한 if문
-});
+};
+volumeRange.addEventListener("input", handleVolumeRangeInput);
+volumeRange.addEventListener("change", handleVolumeRangeChange);
 
 const formatTime = (seconds) => {
   const startIdx = seconds >= 3600 ? 11 : 14;
   return new Date(seconds * 1000).toISOString().substring(startIdx, 19);
 };
-video.addEventListener("timeupdate", () => {
+const handleVideoCurrentTime = () => {
   currentTime.innerText = formatTime(Math.floor(video.currentTime));
-  timeline.value = Math.floor(video.currentTime);
-});
-video.addEventListener("loadedmetadata", () => {
+  timeline.value = Math.floor(video.currentTime); // 타임라인이 매초마다 영상과 함께 앞으로 증가하는 모습 표현
+};
+const handleVideoTotalTime = () => {
   totalTime.innerText = formatTime(Math.floor(video.duration));
-  timeline.max = Math.floor(video.duration);
-});
+  timeline.max = Math.floor(video.duration); // 타임라인의 영상 길이 입력
+};
+video.addEventListener("timeupdate", handleVideoCurrentTime);
+video.addEventListener("loadedmetadata", handleVideoTotalTime);
 
-timeline.addEventListener("input", (event) => {
+const handleTimelineInput = (event) => {
   const {
     target: { value },
   } = event;
-  video.currentTime = value;
-});
+  video.currentTime = value; // 특정 타임라인을 선택하면 그에 따라 비디오 화면이 바뀌도록 함
+};
+timeline.addEventListener("input", handleTimelineInput);
 
-fullScreenBtn.addEventListener("click", () => {
+const handleFullScreenBtn = () => {
   const fullScreen = document.fullscreenElement;
   if (fullScreen) {
     document.exitFullscreen();
@@ -87,13 +103,24 @@ fullScreenBtn.addEventListener("click", () => {
     videoContainer.requestFullscreen();
     fullScreenBtn.innerText = "Exit Full Screen";
   }
+};
+const handleDocumentFull = () => {
+  if (!document.fullscreenElement) {
+    fullScreenBtn.innerText = "Enter Full Screen";
+  }
+}; // ESC를 눌러서 전체화면을 종료했을 때의 변화 감지
+fullScreenBtn.addEventListener("click", handleFullScreenBtn);
+document.addEventListener("fullscreenchange", handleDocumentFull);
+document.addEventListener("keyup", (event) => {
+  if (event.code === "KeyF") {
+    handleFullScreenBtn();
+  }
 });
 
 let controlsTimeoutId;
 let controlsTimeoutIdMove;
 const hideControls = () => videoControls.classList.remove("showing");
-
-video.addEventListener("mousemove", () => {
+const handleVideoMousemove = () => {
   if (controlsTimeoutId) {
     clearTimeout(controlsTimeoutId);
     controlsTimeoutId = null;
@@ -104,7 +131,9 @@ video.addEventListener("mousemove", () => {
   } // 5. 4번이 실행되는 동안 다시 마우스가 움직이는 경우에, 실행중인 4번 없애주기
   videoControls.classList.add("showing"); // 1. 마우스가 비디오에 들어왔을 때 컨트롤바 보이기
   controlsTimeoutIdMove = setTimeout(hideControls, 3000); // 4. 마우스가 비디오에 들어오고, 비디오 안에서 3초 동안 아무 동작을 안 하면 컨트롤바 숨기기
-});
-video.addEventListener("mouseleave", () => {
+};
+const handleVideoMouseleave = () => {
   controlsTimeoutId = setTimeout(hideControls, 1000); // 2. 마우스가 비디오에서 나갔을 때 1초 후에 컨트롤바 숨기기
-});
+};
+video.addEventListener("mousemove", handleVideoMousemove);
+video.addEventListener("mouseleave", handleVideoMouseleave);
