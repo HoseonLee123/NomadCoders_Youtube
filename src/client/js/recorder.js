c2e205affc71546e3b485de36c78679cbebc8434
HoseonLee <hoseon310@gmail.com>
2022-12-14 22:13:00 +0900

    13.1 Video Preview

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,17 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
 const handleStartBtnClick = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true,
-  });
-  console.log(stream);
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    video.srcObject = stream;
+    video.play();
+  } catch (error) {
+    alert("Sorry, but no suitable device was found.");
+  }
 };
 
 startBtn.addEventListener("click", handleStartBtnClick);
